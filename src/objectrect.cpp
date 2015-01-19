3f47f254e3b3781a3e1cd25967a673cd210b31e6
Kevin Velickovic <kevin.velickovic@gmail.com>
2015-01-19 12:57:45 +0100

    ObjectRect cleanning WIP

--- src/objectrect.cpp
+++ src/objectrect.cpp
@@ -1,35 +1,36 @@
 #include "objectrect.h"
 
+/* Constructor */
 ObjectRect::ObjectRect()
 {
-    // Append default points
+    /* Append default points */
     this->points.append(QPointF(0.0, 0.0));
     this->points.append(QPointF(0.0, 0.0));
     this->points.append(QPointF(0.0, 0.0));
     this->points.append(QPointF(0.0, 0.0));
     this->polygon = QPolygonF( this->points );
 
-    // Default id
+    /* Default id */
     this->id = 0;
 
-    // Default pen setup
+    /* Default pen setup */
     this->pen = new QPen(QColor(0, 255, 255, 255), 2);
     this->brush = new QBrush(QColor(0, 255, 0, 50), Qt::SolidPattern);
 
-    // Default projection parameters
+    /* Default projection parameters */
     this->projection_parameters.azimuth = 0.0;
     this->projection_parameters.elevation = 0.0;
     this->projection_parameters.aperture = 0.0;
-
-    this->projection_parameters.points.append(QPointF(0.0, 0.0));
-    this->projection_parameters.points.append(QPointF(0.0, 0.0));
-    this->projection_parameters.points.append(QPointF(0.0, 0.0));
-    this->projection_parameters.points.append(QPointF(0.0, 0.0));
-
     this->projection_parameters.width = 0.0;
     this->projection_parameters.height = 0.0;
 
-    // Default informations
+    /* Default projection points */
+    this->projection_parameters.points.append(QPointF(0.0, 0.0));
+    this->projection_parameters.points.append(QPointF(0.0, 0.0));
+    this->projection_parameters.points.append(QPointF(0.0, 0.0));
+    this->projection_parameters.points.append(QPointF(0.0, 0.0));
+
+    /* Default informations */
     this->info.automatic_status = "None";
     this->info.manual_status = "None";
     this->info.blurred = false;
@@ -37,102 +38,121 @@ ObjectRect::ObjectRect()
     this->info.type = ObjectType::None;
     this->info.sub_type = ObjectSubType::None;
 
-    // Default contour setup
+    /* Default contour setup (First) */
     this->contour_pen = new QPen(QColor(255, 255, 255, 255), 2);
     this->contour = new QGraphicsPolygonItem( this );
     this->contour->setPen( *this->contour_pen );
     this->contour->setBrush( Qt::NoBrush );
 
+    /* Default contour setup (Second) */
     this->contour2_pen = new QPen(QColor(0, 0, 0, 255), 2);
     this->contour2 = new QGraphicsPolygonItem( this );
     this->contour2->setPen( *this->contour2_pen );
     this->contour2->setBrush( Qt::NoBrush );
 
-    // Default resize rect setup
+    /* Default resize rect setup */
     this->resize_rect = new QGraphicsPolygonItem( this );
     this->resize_rect->setPen( *this->pen );
     this->resize_rect->setBrush( Qt::NoBrush );
-
     this->resizeEnabled = true;
 
+    /* Render object */
     this->render();
 }
 
+/* Function to set point 1 */
 void ObjectRect::setPoint1(QPointF point)
 {
+    /* Assign point 1 value */
     this->points[0] = point;
 
-    // Cal rendering procedure
+    /* Render object */
     this->render();
 }
 
+/* Function to set point 2 */
 void ObjectRect::setPoint2(QPointF point)
 {
+    /* Assign point 2 value */
     this->points[1] = point;
 
-    // Cal rendering procedure
+    /* Render object */
     this->render();
 }
 
+/* Function to set point 3 */
 void ObjectRect::setPoint3(QPointF point)
 {
+    /* Assign point 3 value */
     this->points[2] = point;
 
-    // Cal rendering procedure
+    /* Render object */
     this->render();
 }
 
+/* Function to set point 3 in rigid structure, moving other points at the same time, ex: resize edge */
 void ObjectRect::setPoint3_Rigid(QPointF point)
 {
+    /* Backup points */
     QPointF new_p1 = this->points[0];
     QPointF new_p2 = this->points[1];
     QPointF new_p3 = point;
     QPointF new_p4 = this->points[3];
 
+    /* Compute new points */
     new_p2.setY( new_p3.y() );
     new_p4.setX( new_p3.x() );
 
+    /* Clamp x axis */
     if ( (new_p3.x() - new_p2.x()) < 1 )
     {
         new_p3.setX( new_p2.x() + 1 );
         new_p4.setX( new_p3.x() );
     }
 
+    /* Clamp y axis */
     if ( (new_p2.y() - new_p1.y()) < 1 )
     {
         new_p2.setY( new_p1.y() + 1 );
         new_p3.setY( new_p2.y() );
     }
 
-    // Update positions
+    /* Update positions */
     this->setPoints( new_p1, new_p2, new_p3, new_p4 );
 
-    // Cal rendering procedure
+    /* Render object */
     this->render();
 }
 
+/* Function to set point 3 in rigid structure with an offset, moving other points at the same time, ex: resize edge */
 void ObjectRect::setPoint3_Rigid(QPointF point, QPointF offset)
 {
 
+    /* Apply offset */
     QPointF centered (
         point.x() - offset.x(),
         point.y() - offset.y()
     );
 
+    /* Apply modification to point 3 */
     this->setPoint3_Rigid(centered);
 }
 
+/* Function to set point 4 */
 void ObjectRect::setPoint4(QPointF point)
 {
+     /* Assign point 4 value */
     this->points[3] = point;
 
-    // Cal rendering procedure
+    /* Render object */
     this->render();
 }
 
+/* Function to set all the points at the same time */
 void ObjectRect::setPoints(QPointF p1, QPointF p2, QPointF p3, QPointF p4)
 {
 
+    /* Try to compute point 1 if not specified */
     if( p1.x() == 0 || p1.y() == 0 )
     {
         this->setPoint1( QPointF( p2.x(), p4.y() ) );
@@ -140,6 +160,7 @@ void ObjectRect::setPoints(QPointF p1, QPointF p2, QPointF p3, QPointF p4)
         this->setPoint1( p1 );
     }
 
+    /* Try to compute point 2 if not specified */
     if( p2.x() == 0 || p2.y() == 0 )
     {
         this->setPoint2( QPointF( p1.x(), p3.y() ) );
@@ -147,6 +168,7 @@ void ObjectRect::setPoints(QPointF p1, QPointF p2, QPointF p3, QPointF p4)
         this->setPoint2( p2 );
     }
 
+    /* Try to compute point 3 if not specified */
     if( p3.x() == 0 || p3.y() == 0 )
     {
         this->setPoint3( QPointF( p4.x(), p2.y() ) );
@@ -154,6 +176,7 @@ void ObjectRect::setPoints(QPointF p1, QPointF p2, QPointF p3, QPointF p4)
         this->setPoint3( p3 );
     }
 
+    /* Try to compute point 4 if not specified */
     if( p4.x() == 0 || p4.y() == 0 )
     {
         this->setPoint4( QPointF( p3.x(), p1.y() ) );
@@ -161,10 +184,11 @@ void ObjectRect::setPoints(QPointF p1, QPointF p2, QPointF p3, QPointF p4)
         this->setPoint4( p4 );
     }
 
-    // Cal rendering procedure
+    /* Render object */
     this->render();
 }
 
+/* Function to move object at specified coordinates, with an offset */
 void ObjectRect::moveObject(QPointF pos,
                             QPointF offset_1,
                             QPointF offset_2,
@@ -172,156 +196,200 @@ void ObjectRect::moveObject(QPointF pos,
                             QPointF offset_4)
 {
 
-    // Center point by clicking offset
+    /* Center point 1 with offset */
     QPointF centered_point_1 (
         pos.x() - offset_1.x(),
         pos.y() - offset_1.y()
     );
 
+    /* Center point 2 with offset */
     QPointF centered_point_2 (
         pos.x() - offset_2.x(),
         pos.y() - offset_2.y()
     );
 
+    /* Center point 3 with offset */
     QPointF centered_point_3 (
         pos.x() - offset_3.x(),
         pos.y() - offset_3.y()
     );
 
+    /* Center point 4 with offset */
     QPointF centered_point_4 (
         pos.x() - offset_4.x(),
         pos.y() - offset_4.y()
     );
 
-    // Move object point 1
+    /* Move object to new points */
     this->setPoint1(centered_point_1);
     this->setPoint2(centered_point_2);
     this->setPoint3(centered_point_3);
     this->setPoint4(centered_point_4);
 
+
+    /* Render object */
     this->render();
 }
 
+/* Function acting same as moveObject but just return the new points without moving object */
 QVector<QPointF> ObjectRect::simulate_moveObject(QPointF pos, QPointF offset_1, QPointF offset_2, QPointF offset_3, QPointF offset_4)
 {
+    /* Output points variable */
     QVector<QPointF> output;
 
-    // Center point by clicking offset
+    /* Center point 1 with offset */
     QPointF centered_point_1 (
         pos.x() - offset_1.x(),
         pos.y() - offset_1.y()
     );
 
+    /* Center point 2 with offset */
     QPointF centered_point_2 (
         pos.x() - offset_2.x(),
         pos.y() - offset_2.y()
     );
 
+    /* Center point 3 with offset */
     QPointF centered_point_3 (
         pos.x() - offset_3.x(),
         pos.y() - offset_3.y()
     );
 
+    /* Center point 4 with offset */
     QPointF centered_point_4 (
         pos.x() - offset_4.x(),
         pos.y() - offset_4.y()
     );
 
+    /* Append results */
     output.append( centered_point_1 );
     output.append( centered_point_2 );
     output.append( centered_point_3 );
     output.append( centered_point_4 );
 
+    /* Result results */
     return output;
 }
 
+/* Function to get point 1 */
 QPointF ObjectRect::getPoint1()
 {
+    /* Return value */
     return this->points[0];
 }
 
+/* Function to get point 2 */
 QPointF ObjectRect::getPoint2()
 {
+    /* Return value */
     return this->points[1];
 }
 
+/* Function to get point 3 */
 QPointF ObjectRect::getPoint3()
 {
+    /* Return value */
     return this->points[2];
 }
 
+/* Function to get point 4 */
 QPointF ObjectRect::getPoint4()
 {
+    /* Return value */
     return this->points[3];
 }
 
+/* Function to get all points */
 QVector<QPointF> ObjectRect::getPoints()
 {
+    /* Return values */
     return this->points;
 }
 
+/* Function to resize object */
 void ObjectRect::setSize(QSizeF size)
 {
+    /* Backup points */
     QPointF new_p1 = this->points[0];
     QPointF new_p2 = this->points[1];
     QPointF new_p3 = this->points[2];
     QPointF new_p4 = this->points[3];
 
-    // Width
+    /* Compute width */
     new_p2.setX( new_p1.x() + size.width() );
     new_p3.setX( new_p1.x() + size.width() );
 
-    // Height
+    /* Compute height */
     new_p4.setY( new_p1.y() + size.height() );
     new_p3.setY( new_p1.y() + size.height() );
 
-    // Update positions
+    /* Update positions */
     this->setPoints( new_p1, new_p2, new_p3, new_p4 );
 
-    // Cal rendering procedure
+    /* Render object */
     this->render();
 }
 
+/* Function to get object size (initial projection) */
 QSizeF ObjectRect::getSize()
 {
+    /* Baclup values */
     QPointF p1 = this->proj_point_1();
     QPointF p2 = this->proj_point_2();
     QPointF p4 = this->proj_point_4();
 
+    /* Output vriable */
     QSizeF size;
 
+    /* Compute width */
     size.setWidth( p4.x() -  p1.x());
+
+    /* Compute height */
     size.setHeight( p2.y() -  p1.y());
 
+    /* Return value */
     return size;
 }
 
+/* Function to get object size (current projection) */
 QSizeF ObjectRect::getSizeCurrent()
 {
+    /* Baclup values */
     QPointF p1 = this->points[0];
     QPointF p2 = this->points[1];
     QPointF p4 = this->points[3];
 
+    /* Output vriable */
     QSizeF size;
 
+    /* Compute width */
     size.setWidth( p4.x() -  p1.x());
+
+    /* Compute height */
     size.setHeight( p2.y() -  p1.y());
 
+    /* Return value */
     return size;
 }
 
+/* Function to get border width */
 float ObjectRect::getBorderWidth()
 {
+    /* Return value */
     return this->pen->width();
 }
 
+/* Function to set object's ID */
 void ObjectRect::setId(int id)
 {
+    /* Assign value */
     this->id = id;
 }
 
+/* Function to get object's ID */
 int ObjectRect::getId()
 {
+    /* Return value */
     return this->id;
 }
 

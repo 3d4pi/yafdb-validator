561cac79b1f15394f9fffde158cdd0426693ee2e
Kevin Velickovic <kevin.velickovic@gmail.com>
2015-01-21 17:34:07 +0100

    Cleanned utils.cpp & utils.h

--- include/utils.h
+++ include/utils.h
@@ -1,15 +1,17 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+/* Includes */
+#include <QImage>
+#include <QThread>
+
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
-#include <QImage>
 #include <inter-all.h>
 #include <gnomonic-all.h>
 #include "objectrect.h"
 
-#include <QThread>
-
+/* Image info structure */
 struct image_info_struct{
     QImage* image;
     int width;
@@ -17,10 +19,16 @@ struct image_info_struct{
     int channels;
 };
 
+/* Function to convert an OpenCV IplImage into a QImage */
 QImage*  IplImage2QImage(IplImage *iplImg);
+
+/* Function to export an object to disk */
 void exportRect(ObjectRect* rect, image_info_struct image_info, QString destination, float zoom_level = 1.5);
 
+/* Function to clamp a specified value */
 float clamp(float x, float a, float b);
+
+/* Function to clamp a specified radian value */
 float clampRad(float x, float a, float b);
 
 #endif // UTILS_H
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,42 +1,70 @@
+/* Includes */
 #include "utils.h"
 
+/* Function to convert an OpenCV IplImage into a QImage */
 QImage* IplImage2QImage(IplImage *iplImg)
 {
+    /* Save image dismensions */
     int h = iplImg->height;
     int w = iplImg->width;
+
+    /* Get image channels */
     int channels = iplImg->nChannels;
+
+    /* Allocate destination image */
     QImage *qimg = new QImage(w, h, QImage::Format_ARGB32);
+
+    /* Get source image data */
     char *data = iplImg->imageData;
 
+    /* Iterate over image Y axis */
     for (int y = 0; y < h; y++, data += iplImg->widthStep)
     {
+        /* Iterate over image X axis */
         for (int x = 0; x < w; x++)
         {
+            /* Initialize pixel color containers */
             char r = 0;
             char g = 0;
             char b = 0;
             char a = 0;
 
+            /* Channels check */
             if (channels == 1)
             {
+                /* Set red channel */
                 r = data[x * channels];
+
+                /* Set green channel */
                 g = data[x * channels];
+
+                /* Set blue channel */
                 b = data[x * channels];
             }
             else if (channels == 3 || channels == 4)
             {
+                /* Set red channel */
                 r = data[x * channels + 2];
+
+                /* Set green channel */
                 g = data[x * channels + 1];
+
+                /* Set blue channel */
                 b = data[x * channels];
             }
 
+            /* Channels check */
             if (channels == 4)
             {
+                /* Set alpha channel */
                 a = data[x * channels + 3];
+
+                /* Set pixel */
                 qimg->setPixel(x, y, qRgba(r, g, b, a));
             }
             else
             {
+                /* Set pixel */
                 qimg->setPixel(x, y, qRgb(r, g, b));
             }
         }
@@ -44,31 +72,43 @@ QImage* IplImage2QImage(IplImage *iplImg)
     return qimg;
 }
 
+/* Function to export an object to disk */
 void exportRect(ObjectRect *rect, image_info_struct image_info, QString destination, float zoom_level)
 {
-    // Convert points to a QRect and deduce borders sizes
+    /* Copy object */
     ObjectRect* rect_mapped = rect->copy();
 
+    /* Map copied object to source object projection parameters */
     rect_mapped->mapTo(rect->proj_width(),
                        rect->proj_height(),
                        rect->proj_azimuth(),
                        rect->proj_elevation(),
                        rect->proj_aperture() / zoom_level);
+
+    /* Update object projection points */
     rect_mapped->setProjectionPoints();
 
+    /* Get selection from object's points */
     QRect rect_sel(
         QPoint(rect_mapped->proj_point_1().x() + rect_mapped->getBorderWidth(), rect_mapped->proj_point_1().y() + rect_mapped->getBorderWidth()),
         QPoint(rect_mapped->proj_point_3().x() - rect_mapped->getBorderWidth(), rect_mapped->proj_point_3().y() - rect_mapped->getBorderWidth())
     );
 
+    /* Delete temp rect */
     delete rect_mapped;
 
+    /* Create temporary destination image */
     QImage temp_dest(rect->proj_width(), rect->proj_height(), QImage::Format_RGB32);
+
+    /* Determine best number of threads */
     int threads_count = QThread::idealThreadCount();
 
+    /* Check if rect sizes are correct */
     if( rect->getSize().width() >= 1 &&
            rect->getSize().height() >= 1 )
     {
+
+        /* Project gnomonic image */
         lg_etg_apperturep(
 
             ( inter_C8_t * ) image_info.image->bits(),
@@ -87,27 +127,43 @@ void exportRect(ObjectRect *rect, image_info_struct image_info, QString destinat
             threads_count
         );
 
-        // Crop and return image
+        /* Crop and save image */
         QImage element = temp_dest.copy(rect_sel);
         element.save( destination );
     }
 }
 
+/* Function to clamp a specified value */
 float clamp(float x, float a, float b)
 {
+    /* Return result */
     return x < a ? a : (x > b ? b : x);
 }
 
+/* Function to clamp a specified radian value */
 float clampRad(float x, float a, float b)
 {
+    /* Check if value is greather than maximum */
     if(x > b)
     {
+        /* Determine delta between value and maximum */
         float delta = (x - b);
+
+        /* Return maximum + delta */
         return (a + delta);
-    } else if (x < a) {
+    }
+
+    /* Check if value is lesser than minimum */
+    else if (x < a) {
+
+        /* Determine delta between value and minimum */
         float delta = (a - x);
+
+        /* Return minimum + delta */
         return (b - delta);
     } else {
+
+        /* Return value */
         return x;
     }
 }
